9642b36dd3fa86a43f20151494a6fa60fb3f3563
Lucas Alvarez <alvarezlucas2787@gmail.com>
2024-03-26 11:21:09 -0300

    Exercise sumAll completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if ( (x < 0 || y < 0) || (typeof x !== "number" || typeof y !== "number")){
+        return('ERROR')
+    }
 
+    let sum = 0
+
+    const start = x < y ? x : y
+    const finish = x < y ? y : x
+
+
+    for (let index = start; index <= finish; index++){
+        sum += index
+    }
+
+
+    return sum
 };
 
 // Do not edit below this line
