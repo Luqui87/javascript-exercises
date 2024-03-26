cfa02699ce17aef585580514321aec245fde13d8
Lucas Alvarez <alvarezlucas2787@gmail.com>
2024-03-26 10:53:13 -0300

    Exercise removeFromArray completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    const arg = Array.from(arguments);
+    arg.shift();
 
+    for (let i = 0; i < arg.length ; i++){
+
+        array = array.filter(number => number !== arg[i])
+
+    }
+
+    return array;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
